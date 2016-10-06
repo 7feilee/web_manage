e86a5527d3cedb2190da601436ad65b6b1b355a7
S-H-Y-GitHub <932495000@qq.com>
2016-10-06 20:45:12 +0800

    add lib config

--- src/service/Service.java
+++ src/service/Service.java
@@ -1,10 +1,32 @@
 package service;
+import dao.*;
+import model.*;
 
-//Just an example, nothing here
 public class Service
 {
+	private UserDao userDao;
+	private PaperDao paperDao;
+	private NoteDao noteDao;
+	private User user;
+	private Paper paper;
+	private Note note;
+	private Log log;
+	
+	public Service()
+	{
+		super();
+		userDao = new UserDao();
+		paperDao = new PaperDao();
+		noteDao = new NoteDao();
+		user = new User();
+		paper = new Paper();
+		note = new Note();
+		log = new Log();
+	}
+
 	public int login(String username, String password)
 	{
+		
 		return 0;
 	}
 }
