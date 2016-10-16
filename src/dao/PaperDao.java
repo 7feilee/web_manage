1912d2451a0f294ab0638bcd78b17429d3754470
DannieFly <liqifei1@gmail.com>
2016-10-16 19:18:01 +0800

    @ayh and zzy move database to remote mysql server

--- src/dao/PaperDao.java
+++ src/dao/PaperDao.java
@@ -15,7 +15,7 @@ public class PaperDao
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/papermanage", "root", "19951224");
+            conn = DriverManager.getConnection("jdbc:mysql://123.207.154.130:3306/papermanage", "root", "19951224");
             stmt = conn.createStatement();
         }
         catch (SQLException e)
