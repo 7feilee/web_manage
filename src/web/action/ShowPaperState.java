877137b6ed795a1b4de36576e12f908c329c763a
S-H-Y-GitHub <932495000@qq.com>
2016-11-05 15:42:40 +0800

    ajax action层

---
+++ src/web/action/ShowPaperState.java
@@ -0,0 +1,47 @@
+package web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import service.Service;
+public class ShowPaperState extends ActionSupport
+{
+	private int uid;
+	private int pid;
+	private int state;
+	private Service service;
+	
+	public ShowPaperState()
+	{
+		super();
+		service = new Service();
+	}
+	
+	@Override
+	public String execute() throws Exception
+	{
+		state = service.getPaperState(uid,pid);
+		if (state > 0)
+			return SUCCESS;
+		//else
+		return ERROR;
+	}
+	public int getUid()
+	{
+		return uid;
+	}
+	public void setUid(int uid)
+	{
+		this.uid = uid;
+	}
+	public int getPid()
+	{
+		return pid;
+	}
+	public void setPid(int pid)
+	{
+		this.pid = pid;
+	}
+	public int getState()
+	{
+		return state;
+	}
+}
---
+++ web/ajax/showPaperState.jsp
@@ -0,0 +1,6 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<%@ taglib prefix="sj" uri="/struts-jquery-tags" %>
+<%@ taglib prefix="sb" uri="/struts-bootstrap-tags" %>
+
+<s:property value="state"/>
