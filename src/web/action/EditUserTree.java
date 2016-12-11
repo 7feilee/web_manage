13d0f65c93555a3392006373f90eb28a5a632ba1
S-H-Y-GitHub <932495000@qq.com>
2016-12-11 12:38:09 +0800

    进度保存

---
+++ src/web/action/EditUserTree.java
@@ -0,0 +1,29 @@
+package web.action;
+import com.opensymphony.xwork2.ActionSupport;
+import service.Service;
+
+public class EditUserTree extends ActionSupport
+{
+	private String data;
+	private Service service;
+	
+	public EditUserTree()
+	{
+		super();
+		service = new Service();
+	}
+	@Override
+	public String execute() throws Exception
+	{
+		System.out.print(data);
+		return super.execute();
+	}
+//	public String getData()
+//	{
+//		return data;
+//	}
+	public void setData(String data)
+	{
+		this.data = data;
+	}
+}
